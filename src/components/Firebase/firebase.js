f234a38611cc0510fe44864d1810aabfb79f138e
javascript01fan <ejrojas@icloud.com>
2019-11-15 13:08:54 -0500

    .env for devConfig and .env for prodConfigurations added

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,6 +1,6 @@
 import app from 'firebase/app'
 
-const config = {
+const devConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_DATABASE_URL,
@@ -8,6 +8,16 @@ const config = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
+const prodConfig = {
+    apiKey: process.env.REACT_APP_API_PROD_KEY,
+    authDomain: process.env.REACT_APP_AUTH_PROD_DOMAIN,
+    databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+};
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
   //firebase class
 class Firebase {
     constructor() {
