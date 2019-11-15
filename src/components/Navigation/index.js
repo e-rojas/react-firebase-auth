8b3f91ef916afc833c1799aab0c0fbc2da1e4909
javascript01fan <ejrojas@icloud.com>
2019-11-15 12:00:33 -0500

    added

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../constants/routes'
 const Navigation = () => {
     return (
         <div>
-           <h1>Navigation</h1> 
+            <ul>
+                <li>
+                    <Link to={ROUTES.SIGN_IN} >SIGN IN</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.LANDING} >LANDING</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.HOME} >HOME</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.ACCOUNT} >ACCOUNT</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.ADMIN} >ADMIN</Link>
+                </li>
+          </ul>
         </div>
     );
 }
