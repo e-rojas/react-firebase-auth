e5a535e77867d878ba42f515305d1975b9162771
javascript01fan <ejrojas@icloud.com>
2019-11-15 19:29:57 -0500

    signup component with firebase authentication

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,12 +2,12 @@ import app from 'firebase/app'
 import 'firebase/auth'
 
 const devConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    apiKey: process.env.REACT_APP_API_DEV_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DEV_DOMAIN,
+    databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+    projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
 };
 const prodConfig = {
     apiKey: process.env.REACT_APP_API_PROD_KEY,
